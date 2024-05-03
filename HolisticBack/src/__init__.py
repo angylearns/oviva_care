3e3321130c8a110f8aa0e9fb2e2f8cb168b627a6
sandra-medina-luque <153823146+sandra-medina-luque@users.noreply.github.com>
2024-05-03 11:19:27 +0200

    recipe back

--- HolisticBack/src/__init__.py
+++ HolisticBack/src/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
-from src.routes import UserRouter, VideoRouter
+from src.routes import UserRouter, VideoRouter, RecipeRouter
 from flask_cors import CORS
 
 
@@ -14,6 +14,7 @@ def init_app(config):
     
     app.register_blueprint(UserRouter.mainUser, url_prefix='/user')
     app.register_blueprint(VideoRouter.mainVideo, url_prefix='/video')
+    app.register_blueprint(RecipeRouter.mainRecipe, url_prefix='/recipe')
     
     
     return app
