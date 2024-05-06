2c52b133a598ec767a146c362bc454f89c9aed51
Lorena-Rodriguez <lorenarodriguezc@gmail.com>
2024-05-06 10:12:35 +0200

    start login

--- HolisticFront/src/App.jsx
+++ HolisticFront/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import Recipe from './components/recipe/Recipe';
+import Login from './components/login/Login';
 
 function App() {
 
 
   return (
     <>
-      <Recipe/>
+    <Login />
+      {/* <Recipe/> */}
     </>
   )
 }
---
+++ HolisticFront/src/components/login/Login.jsx
@@ -0,0 +1,19 @@
+
+
+
+function Login() {
+
+
+
+
+    
+  return (
+    <div>
+      <h1>Hola, mundo!</h1>
+      <p>Esta es una aplicación simple en React.</p>
+    </div>
+  );
+}
+
+export default Login;
+
