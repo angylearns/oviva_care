c30a4f03bd76229d9ba8644c415a950acff9bfcb
Lorena-Rodriguez <lorenarodriguezc@gmail.com>
2024-05-10 11:17:17 +0200

    add validation fields to login_user form

--- HolisticFront/src/components/login/Login_user.jsx
+++ HolisticFront/src/components/login/Login_user.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'; 
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import '../login/login.css'
 import login_icon from "../../../public/images/icons/login_icon.svg"
@@ -13,7 +13,7 @@ function Login_user() {
     const onSubmit = data => {
         console.log('Login data:', data);
         // Aquí llamarías a tu función handleLogin con los datos del formulario
-        
+
         handleLogin(data, setErrorMessage);
 
     };
@@ -27,27 +27,50 @@ function Login_user() {
             <img src={login_icon} className="login-icon" alt="imagen login" />
             <h1 >Miembro Oviva</h1>
             <p className="text-user">Usuario</p>
-            <input 
+
+            <input
                 className="field-user"
                 type="email"
                 placeholder="Correo Electrónico"
-                {...register("email", { required: true })}
+                {...register("email", {
+                    required: "Este campo es requerido",
+                    pattern: {
+                        value: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
+                        message: "Por favor ingresa un correo electrónico válido"
+                    }
+                })}
             />
-            {errors.email && <p>Este campo es requerido</p>}
+            {errors.email && <p>{errors.email.message}</p>}
 
             <p className="text-password">Contraseña</p>
-             <input 
+            
+
+            <input
                 className="field-password"
                 type="password"
                 placeholder="Contraseña"
-                {...register("password", { required: true })}
+                {...register("password", {
+                    required: "Este campo es requerido",
+                    minLength: {
+                        value: 8,
+                        message: "La contraseña debe tener al menos 8 caracteres"
+                    },
+                    maxLength: {
+                        value: 8,
+                        message: "La contraseña no debe tener más de 8 caracteres"
+                    },
+                    pattern: {
+                        value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/,
+                        message: "La contraseña debe incluir números, letras minúsculas y mayúsculas y símbolos"
+                    }
+                })}
             />
-            {errors.password && <p>Este campo es requerido</p>}
+            {errors.password && <p>{errors.password.message}</p>}
 
             <button type="submit" className="login-button">Ingresar</button>
             {errorMessage && <p>{errorMessage}</p>} {/* Muestra el mensaje de error si existe */}
-            <button  className="goregister-button">¿No eres miembro?, regístrate</button>
-          
+            <button className="goregister-button">¿No eres miembro?, regístrate</button>
+
         </form>
 
     );
