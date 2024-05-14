025d5b9fe1f19f4fce22d77548cd454792a2116a
Ángela Machado <99359053+angylearns@users.noreply.github.com>
2024-05-14 12:07:19 +0200

    Fix navbar and footer style

--- HolisticFront/src/components/footer/Footer.jsx
+++ HolisticFront/src/components/footer/Footer.jsx
@@ -1,25 +1,23 @@
-// TODO: Link everything, try with grid in order to make it responsive the way we planned it on Figma
-
+import { Link, BrowserRouter as Router } from 'react-router-dom';
 import './footer.css';
 
 function Footer() {
-
-
-
     return (
         <footer>
-            <section className="footer_left">
-                <img src="/images/logo_no_words.png" alt="Oviva Logo" className='footer_logo'/>
-                <section>Oviva © 2024</section>
-            </section>
-            <section className="footer_center">
-                <section>¿Quién soy?</section>
-                <section>Política de privacidad</section>
-            </section>
-            <section className="footer_right">
-                <img src="/images/icons/icon_facebook.svg" alt="Facebook logo" />
-                <img src="images/icons/icon_instagram.svg" alt="Instagram logo" />
-            </section>
+            <Router>
+                <section className="footer_left">
+                    <img src="/images/logo_no_words.png" alt="Oviva Logo" className='footer_logo'/>
+                    <section>Oviva © 2024</section>
+                </section>
+                <section className="footer_center">
+                    <Link to="/"><section>¿Quién soy?</section></Link>
+                    <Link to="/"><section>Política de privacidad</section></Link>
+                </section>
+                <section className="footer_right">
+                    <Link to="/" className='link_facebook'><img src="/images/icons/icon_facebook.svg" alt="Facebook logo"/></Link>
+                    <Link to="/" className='link_instagram'><img src="/images/icons/icon_instagram.svg" alt="Instagram logo" /></Link>
+                </section>
+            </Router>
         </footer>
     );
 }
--- HolisticFront/src/components/navbar/Navbar.jsx
+++ HolisticFront/src/components/navbar/Navbar.jsx
@@ -1,95 +1,89 @@
 import React, { useState } from "react";
-// import { BrowserRouter as Router, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Link } from "react-router-dom";
 import "./navbar.css";
 
 function Navbar() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false); 
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
-  
 
   return (
-    <>
+    <Router>
       <nav className="navbar_user_desktop">
-        {/* <section className="navbar_desktop_left">
-                <Link to="/">
-                    <img src="/images/logo_words.png" alt="Oviva Logo" className='navbar_desktop_logo' />
-                </Link>
-            </section>
-            <section className="navbar_desktop_center">
-                <Link to="">Preguntas</Link>
-                <Link to="">Expertos</Link>
-                <Link to="">Blog</Link>
-                {isLoggedIn && <Link to="">Recetas</Link>} 
-                {isLoggedIn && <Link to="">Videos</Link>}
-            </section>
-            <section className="navbar_desktop_right">
-                <Link to="">
-                    <img src="/images/icons/icon_profile_female.svg" alt="User icon" />
-                </Link>
-                <section>ES <span className='language_span'>|</span> EN</section> */}
         <section className="navbar_user_desktop--left">
-          <img
-            src="/images/logo_words.png"
-            alt="Oviva Logo"
-            className="navbar_desktop_logo"
-          />
+          <Link to="/" className="logo_link">
+            <img
+              src="/images/logo_words.png"
+              alt="Oviva Logo"
+              className="navbar_desktop_logo"
+            />
+          </Link>
         </section>
         <section className="navbar_user_desktop--center">
-          <section>Preguntas</section>
-          <section>Expertos</section>
-          <section>Blog</section>
-          {isLoggedIn && <section>Recetas</section>}
-          {isLoggedIn && <section>Videos</section>}
+          <Link to="">Preguntas</Link>
+          <Link to="">Expertos</Link>
+          <Link to="">Blog</Link>
+          {isLoggedIn && <Link to="">Recetas</Link>}
+          {isLoggedIn && <Link to="">Videos</Link>}
         </section>
         <section className="navbar_user_desktop--right">
-          <img src="/images/icons/icon_profile_female.svg" alt="User icon" />
-          <section className="navbar_desktop_language">
-            ES <span className="language_span">|</span> EN
-          </section>
+          {isLoggedIn ? (
+              <section className="navbar_user_desktop--right-left">
+                <section className="navbar_desktop_greeting">¡Hola,<br></br> NOMBRE!</section>
+              <Link to="/logout" className="link_logout">
+                <img src="/images/icons/icon_logout.svg" alt="Logout icon" />
+              </Link>
+              </section>
+              ) : (
+              <Link to="/profile" className="link_profile">
+                <img src="/images/icons/icon_profile_female.svg" alt="User icon" />
+              </Link>
+          )}
         </section>
       </nav>
 
       <nav className="navbar_user_mobile">
-      <section className="navbar_user_mobile--left">
-        <img src="/images/logo_no_words.png" alt="Logo Oviva" />
-      </section>
-      <section className="navbar_user_mobile--right">
-        <img src="/images/icons/icon_profile_female.svg" alt="User icon" className="navbar_mobile_profile_icon" />
-        <input type="checkbox" className="toggle_menu" id="toggle_menu" checked={isOpen} onChange={toggleMenu} />
-        <label htmlFor="toggle_menu" className="navbar_mobile_hamburger_icon">&#9776;</label>
-      </section>
-      <section className={`modal_menu ${isOpen ? 'open' : ''}`}>
-        <ul>
-          <li className="navbar_mobile_language">
-            ES <span className="language_span">|</span> EN
-          </li>
-          <li>Preguntas</li>
-          <li>Expertos</li>
-          <li>Blog</li>
-          {isLoggedIn && <li>Recetas</li>}
-          {isLoggedIn && <li>Videos</li>}
-        </ul>
-        {/* <Router>
-            <ul>
-                <li className="navbar_mobile_language">
-                <Link to="/">ES</Link> <span className="language_span">|</span> <Link to="/">EN</Link>
-                </li>
-                <li><Link to="/preguntas">Preguntas</Link></li>
-                <li><Link to="/expertos">Expertos</Link></li>
-                <li><Link to="/blog">Blog</Link></li>
-                {isLoggedIn && <li><Link to="/recetas">Recetas</Link></li>}
-                {isLoggedIn && <li><Link to="/videos">Videos</Link></li>}
-            </ul>
-        </Router> */}
-      </section>
-    </nav>
-    </>
+        <section className="navbar_user_mobile--left">
+          <img src="/images/logo_no_words.png" alt="Logo Oviva" />
+        </section>
+        <section className="navbar_user_mobile--right">
+          {isLoggedIn ? ( 
+          <React.Fragment>
+            <section className="navbar_mobile_greeting">¡Hola,<br></br> Nombre!</section> 
+            <Link to="/logout" className="link_logout">
+            <img src="/images/icons/icon_logout.svg" alt="Logout icon" />
+            </Link> 
+          </React.Fragment>) : ( <img
+            src="/images/icons/icon_profile_female.svg"
+            alt="User icon"
+            className="navbar_mobile_profile_icon"
+          /> )}
+          <input
+            type="checkbox"
+            className="toggle_menu"
+            id="toggle_menu"
+            checked={isOpen}
+            onChange={toggleMenu}
+          />
+          <label htmlFor="toggle_menu" className="navbar_mobile_hamburger_icon">
+            &#9776;
+          </label>
+        </section>
+        <section className={`modal_menu ${isOpen ? "open" : ""}`}>
+          <ul>
+            <li><Link to="/preguntas">Preguntas</Link></li>
+            <li><Link to="/expertos">Expertos</Link></li>
+            <li><Link to="/blog">Blog</Link></li>
+            {isLoggedIn && <li><Link to="/recetas">Recetas</Link></li>}
+            {isLoggedIn && <li><Link to="/videos">Videos</Link></li>}
+          </ul>
+        </section>
+      </nav>
+    </Router>
   );
-  
 }
 
 export default Navbar;
