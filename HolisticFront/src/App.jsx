81a14f14bd30d1c2b0d360518ff2e2a9518cb115
Ángela Machado <99359053+angylearns@users.noreply.github.com>
2024-05-15 15:03:24 +0200

    Delete a code line in App

--- HolisticFront/src/App.jsx
+++ HolisticFront/src/App.jsx
@@ -6,8 +6,6 @@ import Navbar from './components/navbar/Navbar';
 import Login_user from './components/login/Login_user';
 import Register from './components/register/Register';
 
-import Navbar_admin from './components/navbar/Navbar_admin';
-
 function App() {
 
 
