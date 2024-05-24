ab16f4114c7527892c9593479ca05f7360c303bd
sandra-medina-luque <153823146+sandra-medina-luque@users.noreply.github.com>
2024-05-24 14:10:02 +0200

    home view

--- HolisticFront/src/views/HomeView.jsx
+++ HolisticFront/src/views/HomeView.jsx
@@ -5,22 +5,41 @@ import Symptoms from "../components/symptoms/Symptoms";
 import TestQuestion from "../components/testQuestion/TestQuestion";
 import Pretest from "../components/pretest/Pretest";
 import Footer from "../components/footer/Footer";
+import { useEffect, useState } from "react";
 
+function getCookieValue(name) {
+    const nameEQ = name + "=";
+    const ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
 
 function HomeView() {
-    return (
-        <>
+    const [isLoged, setIsLoged] = useState(false);
+    useEffect(() => {
 
-            <Navbar />
-            <Hero />
-            <TestQuestion />
-            <Pretest />
-            <Symptoms />
-            <Footer />
+        const userType = getCookieValue('user_type');
+
+        if (userType != null){
+            setIsLoged(true);
+        }
+}, []);
+return (
+    <>
+
+        <Navbar />
+        <Hero />
+        {isLoged ? <TestQuestion /> : <Pretest />}
+        <Symptoms />
+        <Footer />
 
 
-        </>
-    );
+    </>
+);
 }
 
 export default HomeView;
