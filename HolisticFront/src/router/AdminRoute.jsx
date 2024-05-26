725cfb473af1f8f959c940aabf9a0b35467bd871
Ángela Machado <99359053+angylearns@users.noreply.github.com>
2024-05-26 19:47:44 +0200

    Admin routes ok

--- HolisticFront/src/router/AdminRoute.jsx
+++ HolisticFront/src/router/AdminRoute.jsx
@@ -1,14 +1,14 @@
+// AdminRoute.jsx
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 import { isAdmin } from '../utils/authUtils';
 
 const AdminRoute = ({ children }) => {
-  // Verifica si el usuario es un administrador sin usar useEffect
   const isAdminUser = isAdmin();
   if (!isAdminUser) {
     return <Navigate to="/" replace />;
   }
-  return <Outlet />;
+  return <>{children}</>;
 };
 
 export default AdminRoute;
--- HolisticFront/src/router/PrivateRoute.jsx
+++ HolisticFront/src/router/PrivateRoute.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 import { useAuth } from '../utils/AuthProvider';
 
-const PrivateRoute = () => {
+const PrivateRoute = ({children}) => {
   const { isAuthenticated } = useAuth();
   if (!isAuthenticated) {
     return <Navigate to="/login" replace />;
   }
-  return <Outlet />;
+  return <>{children}</>;
 };
 
 export default PrivateRoute;
