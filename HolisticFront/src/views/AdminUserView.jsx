9cedd594948968ba810c7d93e3a4ea93a3d41e32
María <ary023ail.com>
2024-05-26 22:16:25 +0200

    fix error AdminUser view in his footer

--- HolisticFront/src/views/AdminUserView.jsx
+++ HolisticFront/src/views/AdminUserView.jsx
@@ -2,19 +2,18 @@
 import Navbar from "../components/navbar/Navbar";
 import AdminPerson from "../components/adminPerson/AdminPerson";
 import Footer from "../components/footer/Footer";
+import "../views/adminUserView.css"
 
 
 function AdminUserView() {
     return (
-        <>
-
-            <div className="main-container">
-                <Navbar />
+        <div className="main-container">
+            <Navbar />
+            <div className="content">
                 <AdminPerson />
-                <Footer />
             </div>
-
-        </>
+            <Footer />
+        </div>
     );
 }
 
