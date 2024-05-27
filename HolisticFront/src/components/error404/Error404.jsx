06cc38caf931b418bd17bc1d032fa4231a70acee
Ángela Machado <99359053+angylearns@users.noreply.github.com>
2024-05-27 15:00:27 +0200

    Change and delete error stuff

--- HolisticFront/src/components/error404/Error404.jsx
+++ HolisticFront/src/components/error404/Error404.jsx
@@ -6,12 +6,10 @@ function Error404() {
     return (
         <section className="error404">
             <img src="./image/error_404.png" className="error404--img" alt="Error 404" />
-            <p>¡Vaya! La página solicitada no existe o necesitas iniciar sesión para acceder.
+            <p>¡Vaya! La página solicitada no existe o necesitas iniciar sesión para acceder.</p>
 
-            Si ya tienes una cuenta, accede <Link to="/login">aqui</Link>
-            Si deseas registrarte, accede <Link to="/register">aqui</Link>
-
-            </p>
+            <p>Si ya tienes una cuenta, accede <Link to="/login">aqui</Link></p>
+            <p>Si deseas registrarte, accede <Link to="/register">aqui</Link></p>
         </section>
     );
 }
