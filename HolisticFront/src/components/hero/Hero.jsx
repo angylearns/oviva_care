ad8eda8582ca0a4bb985646b32c8c08549679383
Ángela Machado <99359053+angylearns@users.noreply.github.com>
2024-05-27 14:52:24 +0200

    Fix hero image

--- HolisticFront/src/components/hero/Hero.jsx
+++ HolisticFront/src/components/hero/Hero.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../hero/hero.css';
-import youtube from "../../../public/images/icons/youtube.jpg";
+import youtube from "../../../public/image/icons/youtube.jpg";
 
 const Hero = () => {
 
