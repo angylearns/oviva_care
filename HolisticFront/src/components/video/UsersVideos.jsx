10af7ab00018eab497200a91497c1123dbe1325e
Ángela Machado <99359053+angylearns@users.noreply.github.com>
2024-05-28 12:46:01 +0200

    Change viedeos thumbnails

--- HolisticFront/src/components/video/UsersVideos.jsx
+++ HolisticFront/src/components/video/UsersVideos.jsx
@@ -7,49 +7,49 @@ const UsersVideos = () => {
       id: 1,
       description: "DAY 1 - PCOS WORKOUT",
       link: "https://www.youtube.com/watch?v=-wR20fVNlKg",
-      thumbnail: "https://res-console.cloudinary.com/dxvsn6u72/thumbnails/v1/image/upload/v1716193560/b3ZpdmEvbXRrd3hucXNxb2E0M3hoYWtvb24=/drilldown",
+      thumbnail: "https://res.cloudinary.com/dxvsn6u72/image/upload/v1716193560/oviva/mtkwxnqsqoa43xhakoon.png",
       category: "Pilates"
     },
     {
       id: 2,
       description: "DAY 2 - PCOS WORKOUT",
       link: "https://www.youtube.com/watch?v=NPww7dqI53Y",
-      thumbnail: "https://res-console.cloudinary.com/dxvsn6u72/thumbnails/v1/image/upload/v1716193559/b3ZpdmEvYjg3cXJkajRyeG4zdjZxbmpyanE=/drilldown",
+      thumbnail: "https://res.cloudinary.com/dxvsn6u72/image/upload/v1716193559/oviva/b87qrdj4rxn3v6qnjrjq.png",
       category: "Yoga"
     },
     {
         id: 3,
         description: "DAY 3 - PCOS WORKOUT",
         link: "https://www.youtube.com/watch?v=P7NFZ26NWuA",
-        thumbnail: "https://res-console.cloudinary.com/dxvsn6u72/thumbnails/v1/image/upload/v1716193559/b3ZpdmEvdzBxaHZiZW54M3hqYWdhcnZtMGI=/drilldown",
+        thumbnail: "https://res.cloudinary.com/dxvsn6u72/image/upload/v1716193559/oviva/w0qhvbenx3xjagarvm0b.png",
         category: "Pilates"
       },
       {
         id: 4,
         description: "DAY 4 - PCOS WORKOUT",
         link: "https://www.youtube.com/watch?v=nj7CILDBpqc",
-        thumbnail: "https://res-console.cloudinary.com/dxvsn6u72/thumbnails/v1/image/upload/v1716193559/b3ZpdmEvbzFybGQ2YzVkYXRpY3Azb29jbmM=/drilldown",
+        thumbnail: "https://res.cloudinary.com/dxvsn6u72/image/upload/v1716193559/oviva/o1rld6c5daticp3oocnc.png",
         category: "Pilates"
       },
       {
         id: 5,
         description: "DAY 5 - PCOS WORKOUT",
         link: "https://www.youtube.com/watch?v=4CSs8sZaHzo",
-        thumbnail: "https://res-console.cloudinary.com/dxvsn6u72/thumbnails/v1/image/upload/v1716193559/b3ZpdmEvZnlpY2kzd2JtOHFqazd2a3BiaGU=/drilldown",
+        thumbnail: "https://res.cloudinary.com/dxvsn6u72/image/upload/v1716193559/oviva/fyici3wbm8qjk7vkpbhe.png",
         category: "Pilates"
       },
       {
         id: 6,
         description: "DAY 6 - PCOS WORKOUT",
         link: "https://www.youtube.com/watch?v=p6cY5Wejx1o",
-        thumbnail: "https://res-console.cloudinary.com/dxvsn6u72/thumbnails/v1/image/upload/v1716193559/b3ZpdmEvdTJoYW9maW5iNHdkY3dobDdvNGw=/drilldown",
+        thumbnail: "https://res.cloudinary.com/dxvsn6u72/image/upload/v1716193559/oviva/u2haofinb4wdcwhl7o4l.png",
         category: "Pilates"
       },
       {
         id: 7,
         description: "DAY 7 - PCOS WORKOUT",
         link: "https://www.youtube.com/watch?v=lW6TvSeWfnk&t=391s",
-        thumbnail: "https://res-console.cloudinary.com/dxvsn6u72/thumbnails/v1/image/upload/v1716193559/b3ZpdmEvbGFhd2U5dm52Ynhua2pzcXZqYWI=/drilldown",
+        thumbnail: "https://res.cloudinary.com/dxvsn6u72/image/upload/v1716193559/oviva/laawe9vnvbxnkjsqvjab.png",
         category: "Pilates"
       }
 
@@ -95,6 +95,7 @@ const UsersVideos = () => {
         {filteredVideos.map((video) => (
           <div key={video.id} className="video-card">
             <h3 className='second-title'>{video.title}</h3>
+            {console.log(video.thumbnail)}
             <img src={video.thumbnail} alt={video.title} className="video-thumbnail" />
             <p className='style-text'>{video.description}</p>
             <a href={video.link} target="_blank" rel="noopener noreferrer">
